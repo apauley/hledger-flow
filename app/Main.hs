64d3df09bf0d6e6631ae7483ba932620b8312516
Andreas Pauley <andreas@pauley.org.za>
2018-07-21 21:17:30 +0200

    Turtle command-line options

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import Turtle
+import Prelude hiding (FilePath, putStrLn)
+import Data.Text.IO (putStrLn)
+
+data Command = Import Text | Report Text deriving (Show)
 
 main :: IO ()
-main = someFunc
+main = do
+  x <- options "Manage your hledger CSV imports and classification: https://github.com/apauley/hledger-makeitso#readme" parser
+  case x of
+    Import t -> putStrLn t
+    Report t -> putStrLn t
+
+parser :: Parser Command
+parser = fmap Import (subcommand "import" "Converts CSV transactions into categorised journal files"  (argText "i" "Import placeholder"))
+     <|> fmap Report (subcommand "report" "Generate Reports" (argText "r" "Report placeholder"))
