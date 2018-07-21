1a6840b5a40478608fa8d5def7122c41d97b7013
Andreas Pauley <andreas@pauley.org.za>
2018-07-21 21:32:49 +0200

    Accept an optional basedir

--- app/Main.hs
+++ app/Main.hs
@@ -6,15 +6,17 @@ import Turtle
 import Prelude hiding (FilePath, putStrLn)
 import Data.Text.IO (putStrLn)
 
-data Command = Import Text | Report Text deriving (Show)
+data Command = Import (Maybe FilePath) | Report (Maybe FilePath) deriving (Show)
 
 main :: IO ()
 main = do
   x <- options "Manage your hledger CSV imports and classification: https://github.com/apauley/hledger-makeitso#readme" parser
   case x of
-    Import t -> putStrLn t
-    Report t -> putStrLn t
+    Import t -> putStrLn "Import: not yet implemented"
+    Report t -> putStrLn "Report: not yet implemented"
 
 parser :: Parser Command
-parser = fmap Import (subcommand "import" "Converts CSV transactions into categorised journal files"  (argText "i" "Import placeholder"))
-     <|> fmap Report (subcommand "report" "Generate Reports" (argText "r" "Report placeholder"))
+parser = fmap Import (subcommand "import" "Converts CSV transactions into categorised journal files" optionalBaseDir)
+     <|> fmap Report (subcommand "report" "Generate Reports" optionalBaseDir)
+
+optionalBaseDir = optional (argPath "basedir" "The hledger-makeitso base directory")
