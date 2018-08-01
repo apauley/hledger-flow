7b225fe671fe8ab36a9968b76fbd05c40f1d3cd1
Andreas Pauley <andreas@pauley.org.za>
2018-08-01 13:37:26 +0200

    opening-closing.journal per account

--- src/CSVImport.hs
+++ src/CSVImport.hs
@@ -54,7 +54,9 @@ importAccounts bankName accountDirs = do
   let accountSrcFiles = onlyFiles $ find (has (text "1-in")) accDir
   let accJournals = importAccountFiles bankName accName rulesFile preprocessScript accountSrcFiles
   let aggregateJournal = accDir </> buildFilename [bankName, accName] "journal"
-  writeJournals aggregateJournal accJournals
+  let equityJournal = accDir </> "opening-closing.journal"
+  liftIO $ touch equityJournal
+  writeJournals aggregateJournal $ (return equityJournal) + accJournals
   return aggregateJournal
 
 importAccountFiles :: Line -> Line -> FilePath -> FilePath -> Shell FilePath -> Shell FilePath
