026228705c6751162af64b804d3b922ad9751a38
Andreas Pauley <andreas@pauley.org.za>
2018-10-13 23:50:48 +0200

    Enable more compilation warnings with -Wall
    And fix them

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,6 @@ module Main where
 
 import Turtle
 import Prelude hiding (FilePath, putStrLn)
-import Data.Text.IO (putStrLn)
 import Data.Maybe
 import Reports
 import CSVImport
@@ -13,8 +12,8 @@ data Command = Import (Maybe FilePath) | Report (Maybe FilePath) deriving (Show)
 
 main :: IO ()
 main = do
-  x <- options "Manage your hledger CSV imports and classification:\nhttps://github.com/apauley/hledger-makeitso#readme" parser
-  case x of
+  cmd <- options "Manage your hledger CSV imports and classification:\nhttps://github.com/apauley/hledger-makeitso#readme" parser
+  case cmd of
     Import maybeBaseDir -> baseDir maybeBaseDir >>= importCSVs
     Report maybeBaseDir -> baseDir maybeBaseDir >>= generateReports
 
@@ -25,4 +24,5 @@ parser :: Parser Command
 parser = fmap Import (subcommand "import" "Converts CSV transactions into categorised journal files" optionalBaseDir)
      <|> fmap Report (subcommand "report" "Generate Reports" optionalBaseDir)
 
+optionalBaseDir :: Parser (Maybe FilePath)
 optionalBaseDir = optional (argPath "basedir" "The hledger-makeitso base directory")
