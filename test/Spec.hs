4d6915a6aa6daaded0d7cc77a46da44e8ed99f01
Andreas Pauley <andreas@pauley.org.za>
2018-10-14 12:53:09 +0200

    Exit with a non-zero exit code on test failures

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Test.HUnit
+import Turtle
 
 import Common
 import CSVImport
@@ -10,4 +11,8 @@ test1 = TestCase (assertEqual "takeLast" [3,5,7] (takeLast 3 [1,3,5,7]))
 tests = TestList [TestLabel "test1" test1]
 
 main :: IO Counts
-main = runTestTT tests
+main = do
+  counts <- runTestTT tests
+  if (errors counts > 0 || failures counts > 0)
+    then exit $ ExitFailure 1
+    else return counts
