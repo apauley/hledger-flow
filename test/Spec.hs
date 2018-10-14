2b24469d58c8a8d84b1c00d201b48b3e42067a7a
Andreas Pauley <andreas@pauley.org.za>
2018-10-14 12:45:12 +0200

    Add a unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+module Main where
+
+import Test.HUnit
+
+import Common
+import CSVImport
+
+test1 = TestCase (assertEqual "takeLast" [3,5,7] (takeLast 3 [1,3,5,7]))
+
+tests = TestList [TestLabel "test1" test1]
+
+main :: IO Counts
+main = runTestTT tests
