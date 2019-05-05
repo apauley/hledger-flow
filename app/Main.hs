3544259b9a021e8b560c96b3bdd2cd761152291b
Andreas Pauley <andreas@pauley.org.za>
2019-05-05 22:58:20 +0200

    Move hledger path, showOpts and sequential into MainParams

--- app/Main.hs
+++ app/Main.hs
@@ -14,15 +14,14 @@ import Hledger.Flow.Common
 import Hledger.Flow.Reports
 import Hledger.Flow.CSVImport
 
-data SubcommandParams = SubcommandParams { maybeBaseDir :: Maybe FilePath
-                                         , hledgerPathOpt :: Maybe FilePath
-                                         , showOpts :: Bool
-                                         , sequential :: Bool
-                                         }
-                      deriving (Show)
+data SubcommandParams = SubcommandParams { maybeBaseDir :: Maybe FilePath } deriving (Show)
 data Command = Import SubcommandParams | Report SubcommandParams deriving (Show)
 
-data MainParams = MainParams { verbose :: Bool } deriving (Show)
+data MainParams = MainParams { verbose :: Bool
+                             , hledgerPathOpt :: Maybe FilePath
+                             , showOpts :: Bool
+                             , sequential :: Bool
+                             } deriving (Show)
 data BaseCommand = Version | Command { mainParams :: MainParams, command :: Command } deriving (Show)
 
 main :: IO ()
@@ -34,24 +33,24 @@ main = do
     Command mainParams' (Report subParams) -> toReportOptions mainParams' subParams >>= generateReports
 
 toImportOptions :: MainParams -> SubcommandParams -> IO IT.ImportOptions
-toImportOptions mainParams' params = do
-  bd <- determineBaseDir $ maybeBaseDir params
-  hli <- hledgerInfoFromPath $ hledgerPathOpt params
+toImportOptions mainParams' subParams' = do
+  bd <- determineBaseDir $ maybeBaseDir subParams'
+  hli <- hledgerInfoFromPath $ hledgerPathOpt mainParams'
   return IT.ImportOptions { IT.baseDir = bd
                           , IT.hledgerInfo = hli
                           , IT.verbose = verbose mainParams'
-                          , IT.showOptions = showOpts params
-                          , IT.sequential = sequential params }
+                          , IT.showOptions = showOpts mainParams'
+                          , IT.sequential = sequential mainParams' }
 
 toReportOptions :: MainParams -> SubcommandParams -> IO RT.ReportOptions
-toReportOptions mainParams' params = do
-  bd <- determineBaseDir $ maybeBaseDir params
-  hli <- hledgerInfoFromPath $ hledgerPathOpt params
+toReportOptions mainParams' subParams' = do
+  bd <- determineBaseDir $ maybeBaseDir subParams'
+  hli <- hledgerInfoFromPath $ hledgerPathOpt mainParams'
   return RT.ReportOptions { RT.baseDir = bd
                           , RT.hledgerInfo = hli
                           , RT.verbose = verbose mainParams'
-                          , RT.showOptions = showOpts params
-                          , RT.sequential = sequential params }
+                          , RT.showOptions = showOpts mainParams'
+                          , RT.sequential = sequential mainParams' }
 
 baseCommandParser :: Parser BaseCommand
 baseCommandParser = (Command <$> verboseParser <*> commandParser)
@@ -64,10 +63,10 @@ commandParser = fmap Import (subcommand "import" "Converts CSV transactions into
 verboseParser :: Parser MainParams
 verboseParser = MainParams
   <$> switch (long "verbose" <> short 'v' <> help "Print more verbose output")
+  <*> optional (optPath "hledger-path" 'H' "The full path to an hledger executable")
+  <*> switch (long "show-options" <> help "Print the options this program will run with")
+  <*> switch (long "sequential" <> help "Disable parallel processing")
 
 subcommandParser :: Parser SubcommandParams
 subcommandParser = SubcommandParams
   <$> optional (argPath "basedir" "The hledger-flow base directory")
-  <*> optional (optPath "hledger-path" 'H' "The full path to an hledger executable")
-  <*> switch (long "show-options" <> help "Print the options this program will run with")
-  <*> switch (long "sequential" <> help "Disable parallel processing")
