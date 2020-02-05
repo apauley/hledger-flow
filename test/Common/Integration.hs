ca3476ddfedf251cee2f77c7cc74d197090a0352
Andreas Pauley <andreas@pauley.org.za>
2020-02-05 21:37:22 +0100

    Remove flakey integration test

--- test/Common/Integration.hs
+++ test/Common/Integration.hs
@@ -28,28 +28,6 @@ testHiddenFiles = TestCase (
      )
   )
 
-testDirOrPwd :: Test
-testDirOrPwd = TestCase (
-  sh (
-      do
-        currentDir <- fmap (\p -> directory (p </> "t")) pwd
-        tmpdir <- using (mktempdir "." "hlflow")
-        let fooDir = collapse $ currentDir </> tmpdir </> "foo/"
-        let barDir = collapse $ currentDir </> tmpdir </> "bar/"
-        mkdir fooDir
-        mkdir barDir
-        d1 <- liftIO $ dirOrPwd Nothing
-        liftIO $ assertEqual "dirOrPwd returns pwd as a fallback" currentDir d1
-        liftIO $ assertEqual "dirOrPwd assumes the fallback is a directory" (directory d1) d1
-        d2 <- liftIO $ dirOrPwd $ Just $ tmpdir </> "foo"
-        liftIO $ assertEqual "dirOrPwd returns the supplied dir - no trailing slash supplied" fooDir d2
-        liftIO $ assertEqual "dirOrPwd assumes the supplied dir is a directory - no trailing slash supplied" (directory d2) d2
-        d3 <- liftIO $ dirOrPwd $ Just $ tmpdir </> "bar/"
-        liftIO $ assertEqual "dirOrPwd returns the supplied dir - trailing slash supplied" barDir d3
-        liftIO $ assertEqual "dirOrPwd assumes the supplied dir is a directory - trailing slash supplied" (directory d3) d3
-     )
-  )
-
 assertSubDirsForDetermineBaseDir :: FilePath -> [FilePath] -> IO ()
 assertSubDirsForDetermineBaseDir expectedBaseDir importDirs = do
   _ <- sequence $ map (assertDetermineBaseDir expectedBaseDir) importDirs
@@ -127,4 +105,4 @@ testFilterPaths = TestCase (
   )
 
 tests :: Test
-tests = TestList [testDirOrPwd, testDetermineBaseDir, testHiddenFiles, testFilterPaths]
+tests = TestList [testDetermineBaseDir, testHiddenFiles, testFilterPaths]
